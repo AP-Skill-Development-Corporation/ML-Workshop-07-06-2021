48083fe04324af4c9b067af8deb3353fe46bca21
GAlekhya123 <51401978+GAlekhya123@users.noreply.github.com>
2021-06-09 18:37:05 +0530

    data structures,functions,modules and packages

---
+++ DAY3/mypack/basic.py
@@ -0,0 +1,4 @@
+def even(a):
+    if a%2==0:
+        return True
+    return False
---
+++ DAY3/mypack/basic1.py
@@ -0,0 +1,5 @@
+def graetest(a,b):
+    if a>b:
+        print("a is greatest")
+    else:
+        print("b is greatest")
